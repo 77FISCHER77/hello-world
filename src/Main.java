c9affad7f73b900f1a0d6e6db50e7d10db8e05ca
77FISCHER77 <atkin21@mail.ru>
2025-05-05 23:47:03 +0500

    изменил значение!!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        int age = 16;
+        int age = 17;
         if (age >= 18) {
             System.out.println("он совершеннолетний");
         } else {

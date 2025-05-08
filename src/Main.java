ca0043b388efb2d4baf7d76bcadba296ff294ba3
77FISCHER77 <atkin21@mail.ru>
2025-05-08 23:36:26 +0500

    подскажите пожалуйста почему 2 программа не работает!!

--- src/Main.java
+++ src/Main.java
@@ -5,123 +5,34 @@ public class Main {
         task3();
         task4();
         task5();
-        task6();
-        task7();
+
     }
 
 
 
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
-        int age = 18;
-        if (age >= 18) {
-            System.out.println("он совершеннолетний");
-        } else {
-            System.out.println("он не достиг совершеннолетия, нужно немного подождать.");
-        }
 
+        int clentOS = 1;
+        String os = clentOS == 0 ? " iOS " : " Android ";
 
+        System.out.println(" Установите версию приложения для " + os + " по ссылке ");
     }
+}
 
-    public static void task2 () {
-        System.out.println("Задача 2");
-        int degreesCelsius = 15;
-        if (degreesCelsius > 5) {
+public static void task2() {
+    System.out.println("Задача 2");
 
-            System.out.println("Сегодня тепло, можно идти без шапки");
-        } else {
-            System.out.println("На улице холодно, нужно надеть шапку");
-        }
-    }
-    public static void task3 () {
-        System.out.println("Задача 3");
-        int speed  = 75;
-        if (speed > 60) {
+    int clientDeviceYear = 2006;
+    String msg = clientDeviceYear <= 2015 ? " облегчёную версию " : " ";
+    String os = clentOS == 0 ? " iOS " : " Android ";
 
-            System.out.println(" Если скорость " + speed + " то  придется заплатить штраф ");
-        } else {
-            System.out.println(" можно ездить спокойно ");
-        }
+    if (clentOS == 0 || clentOS == 1) {
 
-
-    }
-
-    public static void task4 () {
-        System.out.println("Задача 4");
-        int age = 7;
-        if (age >= 2 && age <= 6) {
-
-            System.out.println(" Если возраст человека равен " + age + " то ему нужно ходить в детский сад ");
-        }  else if (age >= 7 && age <=18) {
-            System.out.println(" Если возраст человека равен " + age + " то ему нужно ходить в школу ");
-        }   else if (age >= 18 && age <= 24  ) {
-            System.out.println(" Если возраст человека равен " + age + " то его место в университете ");
-        }  else {
-            System.out.println("Если возраст человека больше 24 лет то ему пора ходить на работу");
-        }
-
-
-
-
-
-
-
-    }
-
-    public static void task5 () {
-        System.out.println("Задача 5");
-        int age = 13;
-        if (age < 5) {
-            System.out.println(" Если возраст ребенка равен " + age + " то ему нельзя кататься на аттракционе ");
-        } else if (age >= 5 && age <= 14) {
-            System.out.println(" Если возраст ребенка равен " + age + " то он может кататься только в сопровождении взрослого ");
-
-        } else if (age > 14) {
-            System.out.println(" Если возраст ребенка равен " + age + " то он может кататься без сопровождения взрослого ");
-        }
-
-
-
-    }
-    public static void task6 () {
-        System.out.println("Задача 6");
-
-        int place = 70;
-        if (place <= 60) {
-            System.out.println(" в вагоне есть сидячие места ");
-        } else if (place > 60 && place <= 102) {
-            System.out.println(" в вагоне есть стоячие места ");
-
-        } else {
-            System.out.println(" в вагоне мест нет ");
-
-
-        }
-
-    }
-
-    public static void task7 () {
-        System.out.println("Задача 7");
-        int number = 3;
-        int one = 1;
-        int two = 2;
-        int three = 3;
-        if (number > one) {
-            System.out.println(" число " + two + " больше одного ");
-        } else if (number > two) {
-            System.out.println(" число " + three + " больше двух");
-
+        System.out.println(" Установите" + msg + " версию приложения для " + os + " по ссылке ");
 
     } else {
-        System.out.println(" число " + one + " меньше 2 и 3"  );
-       }
-
+        System.out.println("SAAS");
     }
-
-
-
-
-
-
 }
